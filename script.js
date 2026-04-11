ac03fc05f05d46964e401a56d5f9ff649c1f78f7
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2026-04-11 12:14:56 +0100

    Level 100 Task  Completed.

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (let episode of episodeList) {
+    const card = document.createElement("div");
+    const title = document.createElement("h2");
+    const image = document.createElement("img");
+    const summary = document.createElement("p");
+
+    title.textContent =
+      episode.name +
+      " - S" +
+      episode.season.toString().padStart(2, "0") +
+      "E" +
+      episode.number.toString().padStart(2, "0");
+    image.src = episode.image.medium;
+    summary.innerHTML = episode.summary;
+    card.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(summary);
+    rootElem.appendChild(card);
+  }
 }
 
 window.onload = setup;
